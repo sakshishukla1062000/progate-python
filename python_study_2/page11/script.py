221367f9ee9fc47cb7094058fcdb5011d83f4d10
sakshishukla1062000 <59702267+sakshishukla1062000@users.noreply.github.com>
2020-06-23 21:44:15 +0530

    progate python study

---
+++ python_study_2/page11/script.py
@@ -0,0 +1,6 @@
+# Create a dictionary with keys and values, and assign it to the items variable
+items={'apple':2,'banana':4,'orange':6}
+# Create a for loop that gets the keys of items
+for item_name in items:
+    print('--------------------------------------------------')
+    print('Each'+item_name+' costs'+ str(items[item_name])+' dollars')
